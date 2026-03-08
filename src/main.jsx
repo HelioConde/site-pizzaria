29b9b126ad471429337a45762d2cd138fe6c4de9
Hélio Conde <helio.conde.contato@gmail.com>
2026-03-08 14:55:35 -0300

    fix: github pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,17 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./styles/global.css";
 
+const basePath = import.meta.env.BASE_URL;
 const params = new URLSearchParams(window.location.search);
 const redirect = params.get("redirect");
 
 if (redirect) {
-  window.history.replaceState(null, "", "/" + redirect);
+  window.history.replaceState(null, "", `${basePath}${redirect}`);
 }
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter basename={basePath}>
       <App />
     </BrowserRouter>
   </StrictMode>
